bf21c5bbce0270c0fc39a0ace65849bcb965b861
leonardo-cavalini <leo.cavalini.2019@gmail.com>
2020-07-29 16:46:14 -0300

    Commit inicial do projeto

---
+++ src/components/Menu/components/ButtonLink/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function ButtonLink(props){
+    //props => { className = 'fjiebvpiw H0A AJH0V', href:"/" }
+    
+    console.log(props);
+    return (
+        <a href={props.href} className={props.className}>    
+         Novo vídeo  
+        </a>
+        );
+}
+
+export default ButtonLink;
