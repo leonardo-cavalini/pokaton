9fd8a13335ad059b61dc07f23daaa272f861ba8f
leonardo-cavalini <leo.cavalini.2019@gmail.com>
2020-08-02 19:44:42 -0300

    ee

--- src/components/pages/cadastro/categoria/index.js
+++ src/components/pages/cadastro/categoria/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { useState} from 'react';
 import PageDefault from '../../../PageDefault';
 import { Link } from 'react-router-dom';
 
 function CadastroCategoria(){
+  let [nomeDaCategoria, setNomeDaCategoria] = useState('Valor Inicial');
+
     return(
       <PageDefault>
-        <h1>Cadastro de Categoria</h1>
+        <h1>Cadastro de Categoria: {nomeDaCategoria}</h1>
 
       <form>
         <label>
           Nome da Categoria
 
-          <input type= "Text"/>
+          <input type= "Text"
+          value={nomeDaCategoria}
+          onChange={function sabonete(infoDoEvento){
+            console.log('[nomeDaCategoria]', nomeDaCategoria);
+            console.log('[infoDoEvento]', infoDoEvento.target.value);
+            setNomeDaCategoria(infoDoEvento.target.value);
+  }}
+          />
         </label>
 
         <button>
